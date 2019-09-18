e377ce4691dc9123d9a88ae6661391d1abc37c7c
dirlin <1462761521@qq.com>
2019-09-18 20:35:32 +0800

    add dependency rxjava3

--- ss-service/src/main/java/com/rainbow/service/SysUserService.java
+++ ss-service/src/main/java/com/rainbow/service/SysUserService.java
@@ -1,23 +1,27 @@
 package com.rainbow.service;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.rainbow.dao.mapper.SysUserMapper;
 import com.rainbow.domain.SysUser;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
-public class SysUserService{
-
-    @Autowired
-    private SysUserMapper sysUserMapper;
+public class SysUserService extends ServiceImpl<SysUserMapper, SysUser> {
 
     public Integer addSysUser(SysUser user) {
+
         user.setOperator("system");
         user.setOperateIp("192.168.1.1");
         user.setOperateTime(new Date());
-        return sysUserMapper.insert(user);
+
+        if (save(user)) {
+            return 1;
+        } else {
+            return 0;
+        }
+
     }
 
     public void updateSysUser(SysUser sysUser) {
