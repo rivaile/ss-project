f4d5926f2f1ad684afd5b24901cde8ebdb8b99c9
dirlin <1462761521@qq.com>
2019-09-21 19:07:35 +0800

    用户更新

--- ss-service/src/main/java/com/rainbow/service/SysUserService.java
+++ ss-service/src/main/java/com/rainbow/service/SysUserService.java
@@ -1,5 +1,7 @@
 package com.rainbow.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.google.common.base.Preconditions;
 import com.rainbow.dao.mapper.SysUserMapper;
 import com.rainbow.domain.SysUser;
 import com.rainbow.enums.ReturnCode;
@@ -29,11 +31,27 @@ public class SysUserService extends BaseService<SysUserMapper, SysUser> implemen
         user.setOperateIp("192.168.1.1");
         user.setOperateTime(new Date());
 
+        // todo: 操作日志 > 可以使用aop实现
         if (save(user))
             return Response.success("注册成功");
         else
-            return Response.error(ReturnCode.AUTH_SERVER_1010001,"dsds");
-        // todo: 操作日志 > 可以使用aop实现
+            return Response.error(ReturnCode.AUTH_SERVER_1010006, "注册失败");
+    }
+
+    public void deleteByUser() {
+
+    }
+
+
+    public void updateUser(SysUserReq userReq) {
+        // todo: 电话邮箱校验
+        SysUser user = getById(userReq.getId());
+        BeanUtils.copyProperties(userReq,
+                Preconditions.checkNotNull(user, "待更新的用户不存在"));
+        user.setOperator("system");
+        user.setOperateIp("192.168.1.1");
+        user.setOperateTime(new Date());
+
     }
 
     public void updateSysUser(SysUser sysUser) {
